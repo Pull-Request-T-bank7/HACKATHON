0dd7379dd67fd238b109c45bf75089a632c5e09a
Иван Горбунов <muervos@muervos.muervos>
2025-04-20 00:45:16 +0300

    Добавление бургер меню и исправленных header и footer

--- Frontend/src/components/Search.jsx
+++ Frontend/src/components/Search.jsx
@@ -1,19 +1,27 @@
-import Found from "../Layout/assets/search.svg"
+import Found from "../Layout/assets/search.svg";
+import Like from "../Layout/assets/image 6.png";
 
-function Search(){
-    return(
-    <div className='search_container'>
+function Search() {
+  return (
+    <div className='search_like_container'>
+      <div className='search_container'>
         <input
-            className='search_form'
-            type="text"
-            placeholder="Найти мероприятие"
+          className='search_form'
+          type="text"
+          placeholder="Найти мероприятие"
         />
         <img 
-            src={Found} 
-            className='search_icon'
-            alt="Поиск"
+          src={Found} 
+          className='search_icon'
+          alt="Поиск"
         />
+      </div>
+      <img 
+        src={Like}
+        alt="Лайк"
+        className='like_img'
+      />
     </div>
-    )
+  );
 }
-export default Search
+export default Search;
